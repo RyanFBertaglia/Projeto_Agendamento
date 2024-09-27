ff586ed9aa24b7dddc51eb8f3f116dc8d5f11d5b
Ryan Fernandes Bertaglia <ryanfernandesbertaglia@gmail.com>
2024-09-27 18:08:02 -0300

    Atualização para pegar dados do campo

---
+++ envioDados.js
@@ -0,0 +1,4 @@
+document.getElementById('diaEscolhido').addEventListener('click', function() {
+    const valor = document.getElementById('DD').value;
+    console.log(valor);
+});
--- index.js
+++ index.js
@@ -1,28 +1,13 @@
 $("#DD").flatpickr({
     minDate: "today",
     maxDate: "dd/mm/2025",
-    //enableTime: true,
     "disable": [
         function(date) {
-            // return true to disable
             return (date.getDay() === 0 || date.getDay() === 6);
 
         }
     ],
     "locale": {
-        "firstDayOfWeek": 0 // start week on Monday
+        "firstDayOfWeek": 0
     },
-   // minuteIncrement: "30",
-    //time_24hr: "true",
-});
-
-
-$("#AA").flatpickr({
-    
-    enableTime: true,
-    noCalendar: true,
-    dateFormat: "H:i",
-    minTime: "16:00",
-    maxTime: "22:30",
-
 });
