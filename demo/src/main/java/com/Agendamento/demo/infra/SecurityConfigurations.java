e7b084c6407be263b4db443f5178eae0c06aa4b5
Ryan Fernandes Bertaglia <ryanfernandesbertaglia@gmail.com>
2025-01-23 10:49:07 -0300

    Adição CORS

--- demo/src/main/java/com/Agendamento/demo/infra/SecurityConfigurations.java
+++ demo/src/main/java/com/Agendamento/demo/infra/SecurityConfigurations.java
@@ -5,14 +5,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -26,8 +33,8 @@ public class SecurityConfigurations {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity
-                .csrf(csrf -> csrf.disable())
+        return httpSecurity.cors(Customizer.withDefaults()).
+                csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
@@ -48,5 +55,15 @@ public class SecurityConfigurations {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "file:///D:/Programação/agendamentoFront/Projeto_Agendamento/Cad-Log/Login/login.html"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- demo/src/test/java/com/Agendamento/demo/Model/HorariosService/SalvaHorarioTest.java
+++ demo/src/test/java/com/Agendamento/demo/Model/HorariosService/SalvaHorarioTest.java
@@ -16,41 +16,40 @@ class SalvaHorarioTest {
 
     @Test
     void Limites() {
-        assertTrue(salvaHorario.salvar(7, "2025/03/25", "23:59"));
+        assertTrue(salvaHorario.salvar(7, "2025-03-25", "23:59"));
         assertThrows(DataEnviadaErrada.class, () -> {
-            salvaHorario.salvar(1, "2025/03/32", "00:00");
+            salvaHorario.salvar(1, "2025-03-32", "00:00");
         });
-        assertTrue(salvaHorario.salvar(3, "2025/12/31", "00:01"));
+        assertTrue(salvaHorario.salvar(3, "2025-12-31", "00:01"));
         assertThrows(DataEnviadaErrada.class, () -> {
-            salvaHorario.salvar(14, "2025/00/27", "24:01");
+            salvaHorario.salvar(14, "2025-00-27", "24:01");
         });
-        assertTrue(salvaHorario.salvar(1, "2025/03/30", "13:00"));
+        assertTrue(salvaHorario.salvar(1, "2025-03-30", "13:00"));
         assertThrows(UserNaoEncontrado.class, () -> {
-            salvaHorario.salvar(0, "2025/03/17", "24:00");
+            salvaHorario.salvar(0, "2025-03-17", "24:00");
         });
         assertThrows(DataEnviadaErrada.class, () -> {
-            salvaHorario.salvar(15, "2025/03/32", "24:01");
+            salvaHorario.salvar(15, "2025-03-32", "24:01");
         });
-
     }
 
     @Test
     void formatoData(){
         assertThrows(DataEnviadaErrada.class, () -> {
-            salvaHorario.salvar(1, "10/03/2025", "23:00");
+            salvaHorario.salvar(1, "10-03-2025", "23:00");
         });
         assertThrows(DataEnviadaErrada.class, () -> {
-            salvaHorario.salvar(1, "2025/03/0", "24:00");
+            salvaHorario.salvar(1, "2025-03-0", "24:00");
         });
         assertThrows(DataEnviadaErrada.class, () -> {
-            salvaHorario.salvar(1, "2025/033/24", "12:00");
+            salvaHorario.salvar(1, "2025-033-24", "12:00");
         });
         assertThrows(DataEnviadaErrada.class, () -> {
-            salvaHorario.salvar(1, "2g25/03/15", "19:000");
+            salvaHorario.salvar(1, "2g25-03-15", "19:000");
         });
-        assertTrue(salvaHorario.salvar(1, "2025/03/30", "14:00"));
+        assertTrue(salvaHorario.salvar(1, "2025-03-30", "14:00"));
         assertThrows(DataEnviadaErrada.class, () -> {
-            salvaHorario.salvar(1, "2025/0/31", "14:53");
+            salvaHorario.salvar(1, "2025-0-31", "14:53");
         });
     }
 }
